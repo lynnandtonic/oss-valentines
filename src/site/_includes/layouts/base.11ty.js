216c2ccfea8a6f7360c156759d61ed2c4f736554
Lynn Fisher <lynn.fisher@netlify.com>
2022-02-11 13:56:31 -0700

    add title and description

--- src/site/_includes/layouts/base.11ty.js
+++ src/site/_includes/layouts/base.11ty.js
@@ -25,8 +25,8 @@ exports.render = function(data) {
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta name="description" content="">
-  <title></title>
+  <meta name="description" content="To show our love for the OSS community, join us in sending a valentine—a token of appreciation—to your favorite open source developers and projects.">
+  <title>Open source, Open hearts</title>
   <link rel="icon" type="image/png" href="/img/favicon-32x32.png" sizes="32x32">
   <link rel="icon" type="image/png" href="/img/favicon-16x16.png" sizes="16x16">
   <meta property="og:type" content="website">
