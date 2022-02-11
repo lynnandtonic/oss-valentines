af30e365c5344a0f158eee2a5901d2aedf073091
Lynn Fisher <lynn.fisher@netlify.com>
2022-02-11 11:06:06 -0700

    button group

--- netlify/functions/partials/card.js
+++ netlify/functions/partials/card.js
@@ -12,8 +12,10 @@ module.exports = (data) => {
         <p class="center">Send this URL to your OSS valentine.</p>
         <!-- NOTE: Clicking this should probably copy the URL to your clipboard and not link to the page? -->
         <a href="/card/${data.path}" class="share-link">https://oss.love/card/${data.path}</a>
-        <button class="copy-url">Copy</button>
-        <a class="button tweet-link" href="https://twitter.com/intent/tweet?text=I can't keep my feelings a secret... I just have to share this valentine for @${data.recipientName} with the world!&url=https://oss.love/card/${data.path}">Tweet</a>
+        <div class="button-group">
+          <button class="copy-url">Copy</button>
+          <a class="button tweet-link" href="https://twitter.com/intent/tweet?text=I can't keep my feelings a secret... I just have to share this valentine for @${data.recipientName} with the world!&url=https://oss.love/card/${data.path}">Tweet</a>
+        </div>
       </section>
       
       
