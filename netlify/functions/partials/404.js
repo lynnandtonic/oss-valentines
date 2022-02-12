c7305fff7b4dd002f4f17b30d389471d714fefe2
Phil Hawksworth <phil@netlify.com>
2022-02-12 20:07:57 +0000

    simple 404 for missing cards, and campaign tag

---
+++ netlify/functions/partials/404.js
@@ -0,0 +1,15 @@
+module.exports = () => {
+
+
+  return `
+  <main class="container">
+    <div class="content">
+      <section class="cta cta-info">
+        <h2>Where is the love?</h2>
+        <p class="center">
+          I'm sure somebody is thinking of you. Maybe <a href="/">sending some love</a> of your own might give them a gentle nudge!
+        </p>
+      </section>
+    </div>
+  </main>`;
+};
