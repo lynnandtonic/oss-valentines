2583f27bd332acc7437559fa396989faa7ba4b97
Phil Hawksworth <phil@netlify.com>
2022-02-12 17:36:50 +0000

    path persistence after login

--- netlify/functions/auth-login.ts
+++ netlify/functions/auth-login.ts
@@ -1,17 +1,28 @@
 import { Handler } from '@netlify/functions';
 import { getWebFlowAuthorizationUrl } from '@octokit/oauth-methods';
+import { serialize } from 'cookie';
 
-export const handler: Handler = async () => {
+export const handler: Handler = async (event) => {
+
+  const redirectCookie = serialize('nf-authed-path',  JSON.stringify({route: event.headers.referer}), {
+    secure: true,
+    httpOnly: true,
+    sameSite: true,
+    maxAge: 1000 * 60 * 60 * 24 * 14, // two weeks
+  });
+
   const { url } = await getWebFlowAuthorizationUrl({
     clientType: 'oauth-app',
     clientId: process.env.GITHUB_APP_CLIENT_ID,
-    scopes: ['read:user'],
-    // redirectUrl: "https://oss-valentine-e28c66.netlify.live/customize/div-without-you/"
+    scopes: ['read:user', 'read:org'],
   });
 
   return {
     statusCode: 301,
-    headers: { Location: url },
+    headers: {
+      'Set-Cookie': redirectCookie,
+      Location: url,
+    },
     body: JSON.stringify({ status: 'Redirecting...' }),
   };
 };
